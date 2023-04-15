7faa79581f5b3cfefa67396871e6dd588d9bc06f
Jun <kumejun2000@gmail.com>
2023-04-15 22:00:44 +0900

    simulatorの追加

---
+++ simulator.py
@@ -0,0 +1,32 @@
+from tqdm import tqdm
+from copy import deepcopy
+from collector import Collector
+from policy.qlearning import QLearning
+from policy.sarsa import Sarsa
+
+class Simulator:
+    def __init__(self, sim, epi, env):
+        self.sim = sim
+        self.epi = epi
+        self.env = env
+        self.policy = Sarsa()
+        self.collector = Collector(sim, epi)
+
+    def run(self):
+        for s in tqdm(range(self.sim)):
+            self.policy.reset()
+            self.collector.reset()
+            for e in range(self.epi):
+                state = self.env.reset()
+                terminated, truncated, total_reward, total_step = False, False, 0, 0
+                while not (terminated or truncated):
+                    action = self.policy.action(state)
+                    next_state, reward, terminated, truncated, info = self.env.step(action)
+                    self.policy.update(state, action, reward, next_state, (terminated or truncated))
+                    state = deepcopy(next_state)
+                    total_reward += reward
+                    total_step += 1
+                self.collector.collect_episode_data(total_reward, total_step)
+            self.collector.save_episode_data()
+        self.collector.save_simulation_data()
+        self.env.close()
